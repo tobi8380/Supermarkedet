2393fb858f0de2110930f2d4a0f5cded4d50a78c
Hylsebeck <58297386+Hylsebeck@users.noreply.github.com>
2021-01-14 10:10:33 +0100

    Update gui.py

--- gui.py
+++ gui.py
@@ -18,18 +18,18 @@ def register():
     username = StringVar()
     password = StringVar()
 
-    Label(register_screen, text="Please enter details below", bg="blue").pack()
+    Label(register_screen, text="Indtast oplysninger", bg="blue").pack()
     Label(register_screen, text="").pack()
-    username_lable = Label(register_screen, text="Username * ")
+    username_lable = Label(register_screen, text="Brugernavn * ")
     username_lable.pack()
     username_entry = Entry(register_screen, textvariable=username)
     username_entry.pack()
-    password_lable = Label(register_screen, text="Password * ")
+    password_lable = Label(register_screen, text="Adgangskode * ")
     password_lable.pack()
     password_entry = Entry(register_screen, textvariable=password, show='*')
     password_entry.pack()
     Label(register_screen, text="").pack()
-    Button(register_screen, text="Register", width=10, height=1, bg="blue", command = register_user).pack()
+    Button(register_screen, text="Registrer bruger", width=10, height=1, bg="blue", command = register_user).pack()
 
 
 # Designing window for login
