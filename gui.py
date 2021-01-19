e1df35b1b731a5e6bcf24f98295c4d6cd96a43a4
Hylsebeck <58297386+Hylsebeck@users.noreply.github.com>
2021-01-19 10:32:44 +0100

    Update gui.py

--- gui.py
+++ gui.py
@@ -4,8 +4,11 @@ import os
 import login
 from data import Super_data
 
-class supermarket:
+class supermarket_gui(ttk.Frame):
 def __init__(self, master=None):
     ttk.Frame.__init__(self, master)
 
         self.build_GUI()
+
+def build_GUI():
+    pass
