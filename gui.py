f8a3c8c5df3b30a0ca4b12afbddc0e31c6d6c3d0
Henrik <hhn0707@gmail.com>
2021-01-21 10:45:40 +0100

    opret bruger

--- gui.py
+++ gui.py
@@ -51,7 +51,6 @@ class supermarket_gui(ttk.Frame):
         self.password_login_entry.delete(0, "end")
 
     def create_user_screen(self):
-
         self.password_register = tk.StringVar()
         self.username_register = tk.StringVar()
 
@@ -67,6 +66,11 @@ class supermarket_gui(ttk.Frame):
         self.username_register_entry = tk.Entry(create_user_screen, textvariable=self.password_register)
         self.username_register_entry.pack()
 
+        tk.Button(create_user_screen, text="Opret bruger", width=10, height=1, command=self.create_user).pack()
+
+    def create_user(self):
+        super_data.register_user("username_register_entry", "password_register_entry")
+        print("bruger oprettet")
 
     def build_GUI(self):
         self.tabs = ttk.Notebook(self)
